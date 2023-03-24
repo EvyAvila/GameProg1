6bade4e4e6d4b8e47b0385c1e118e4a7c49c6d3d
Evy Avila <384036@colum.edu>
2023-03-23 21:36:35 -0500

    Fixing upload

--- Graphs/GraphApp/GraphApp/GraphForm.cs
+++ Graphs/GraphApp/GraphApp/GraphForm.cs
@@ -15,56 +15,107 @@ namespace GraphApp
     public partial class GraphForm : Form
     {
         int[,] adjMatrix;
-        List<Tuple<int,int>> vertex; //https://stackoverflow.com/questions/19231460/define-a-list-like-listint-string
+        List<Tuple<int, int>> vertex; //https://stackoverflow.com/questions/19231460/define-a-list-like-listint-string
 
-        double radius;
-
-        public GraphForm( int[,] _adjMatrix)
+        public GraphForm(int[,] _adjMatrix)
         {
             InitializeComponent();
             adjMatrix = _adjMatrix;
-            vertex=  new List<Tuple<int, int>>();
+            vertex = new List<Tuple<int, int>>();
         }
 
-        private void DrawVertices(object sender, PaintEventArgs e)
+
+
+
+        private void CheckForVertices()
         {
-            Brush brush= new SolidBrush(Color.Red); 
+            for (int col = 1; col < 10; col++) //look through (col,row) up down, left right
+            {
+                for (int row = 1; row < 10; row++)
+                {
+                    int current = adjMatrix[col, row];
+
+                    if (current == 1)
+                    {
+                        vertex.Add(new Tuple<int, int>(row, col));
+                    }
+                }
+            }
+            vertex.Sort();
+        }
+
+
+        private void DrawPoints(object sender, PaintEventArgs e)
+        {
+            Pen pen = new Pen(Color.Red, 2);
+            Pen LinePen = new Pen(Color.Blue, 2);
+
+            List<Point> pointLocation = new List<Point>();
 
             foreach (var v in vertex)
             {
-                CreateGraph(v.Item1, v.Item2);
-                e.Graphics.FillRectangle(brush, v.Item1, v.Item2, 1, 1); //from https://stackoverflow.com/questions/761003/draw-a-single-pixel-on-windows-forms
-                e.Graphics.RotateTransform((float)radius);
+                pointLocation.Add(new Point(v.Item1, v.Item2));
             }
-        }
-        
-        //Might not be right
-        private void CreateGraph(int col, int row)
-        {
-            double Degrees = 2 * int.Parse(adjMatrix.GetValue(col, row).ToString()); 
-            double Edges = Degrees / 2;
-            int origin = col + row;
-            radius = Edges / 2; //?
-            double theta = Math.Atan(Math.PI / 180); //https://www.oreilly.com/library/view/c-cookbook/0596003390/ch01s15.html
-            double EndPoint = Math.Cos(col) / Math.Sin(row) * radius; 
 
+            Point point = new Point(0, 0);
+            Point current = new Point(0, 0);
+            Point next = new Point(0, 0);
+            Point checkPoint;
+
+
+            if (pointLocation.Count > 1)
+            {
+                checkPoint = new Point(pointLocation[1].X, pointLocation[1].Y);
+            }
+            else
+            {
+                checkPoint = new Point(0, 0);
+            }
+
+            List<Point> GetPoints = new List<Point>();
+            for (int i = 0; i < vertex.Count; i++)
+            {
+                point = new Point(pointLocation[i].X, pointLocation[i].Y);
+
+                if (point.X != checkPoint.X || point.Y != checkPoint.Y)
+                {
+                    e.Graphics.DrawEllipse(pen, new Rectangle(point.X * 30, point.Y * 30, 4, 4)); //Help from Dez
+
+                    //GetPoints.Add(new Point(point.X * 30, point.Y * 30));
+                }
+            }
+
+            for (int i = 0; i < vertex.Count; i++)
+            {
+                point = new Point(pointLocation[i].X, pointLocation[i].Y);
+                GetPoints.Add(new Point(point.X * 30, point.Y * 30));
+            }
+
+            //Draw the lines            
+            for (int i = 0; i < GetPoints.Count - 1; i++)
+            {
+                if (GetPoints[i].X != GetPoints[i + 1].X || GetPoints[i].Y != GetPoints[i + 1].Y)
+                {
+                    current = GetPoints[i];
+                    next = GetPoints[i + 1];
+                }
+                else
+                {
+                    current = GetPoints[i];
+                    next = GetPoints[i + 1];
+                }
+
+                e.Graphics.DrawLine(LinePen, current, next);
+            }
+
+            //e.Graphics.DrawLine(LinePen, next, GetPoints[0]); //end the line
         }
 
         private void GraphForm_Paint(object sender, PaintEventArgs e) //Help from https://www.makeuseof.com/c-sharp-windows-form-graphics/
         {
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    if (int.Parse(adjMatrix.GetValue(i, j).ToString()) != 0 && i != j)
-                    {
-                        vertex.Add( new Tuple<int, int>(i, j));
-                        //CreateGraph(i, j);
-                    }
-                }
-            }
-            
-            DrawVertices(sender, e);
+            CheckForVertices();
+            DrawPoints(sender, e);
         }
+
     }
 }
